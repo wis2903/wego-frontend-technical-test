03d49251bb6489837108a3923b8953cc82247305
Whiskey <wis290394@gmail.com>
2023-03-18 15:51:21 +0700

    add tab component

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import Button from './components/button';
 import Input from './components/input';
+import Tab from './components/tab';
 import SearchIcon from './resources/images/search.png';
 
 const App = (): JSX.Element => {
   return (
     <div>
-      <Button primary>Show More</Button>
+      <Button outline>Show More</Button>
       <br />
-      <Input iconUrl={SearchIcon} placeholder='Enter restaurant name...'/>
+      <br />
+      <div style={{ width: '360px' }}>
+        <Input iconUrl={SearchIcon} placeholder='Enter restaurant name...' />
+      </div>
+      <br />
+      <Tab
+        items={[
+          {
+            title: 'All',
+            value: 1,
+            active: true,
+          },
+          {
+            title: 'Sushi',
+            value: 2,
+          },
+          {
+            title: 'Pizza',
+            value: 3,
+          },
+        ]}
+      />
     </div>
   );
 };
