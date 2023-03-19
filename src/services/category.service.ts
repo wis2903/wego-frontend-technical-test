8581b28abc38fb7ab81587d227d36472f25a887b
Whiskey <wis290394@gmail.com>
2023-03-19 07:22:06 +0700

    add definition for services and helpers

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -3,10 +3,16 @@ import config from '../config';
 import { parseCategoryData } from '../helpers/data.helper';
 
 class CategoryService {
+    /**
+     * Fetching categories from API
+     * @returns Promise for list of categories
+     */
     list(): Promise<ICategoryDetails[]> {
         return new Promise((resolve, reject) => {
             axios.get(`${config.api.apiBaseUrl}/${config.api.categoryAPIEndpoint}`).then(res => {
+                // If 'data' field doesn't exist in the response or not in the right data type -> throw error
                 if (!Array.isArray(res.data)) reject(new Error('categories list not found'));
+                // Parse 'data' from API to ICategoryDetails list
                 resolve(res.data.map((item: Record<string, unknown>) => parseCategoryData(item)));
             }).catch(e => {
                 reject(e);
--- src/services/food.service.ts
+++ src/services/food.service.ts
@@ -3,13 +3,17 @@ import config from '../config';
 import { parseFoodData } from '../helpers/data.helper';
 
 class FoodService {
+    /**
+     * Fetching list of foods from API
+     * @returns Promise for list of foods
+     */
     list(): Promise<IFoodDetails[]> {
         return new Promise((resolve, reject) => {
             axios.get(`${config.api.apiBaseUrl}/${config.api.foodAPIEndpoint}`).then(res => {
+                // If 'data' field doesn't exist in the response or not in the right data type -> throw error
                 if (!Array.isArray(res.data)) reject(new Error('foods list not found'));
-                setTimeout(() => {
-                    resolve(res.data.map((item: Record<string, unknown>) => parseFoodData(item)));
-                }, 3000);
+                // Parse 'data' from API to IFoodDetails list
+                resolve(res.data.map((item: Record<string, unknown>) => parseFoodData(item)));
             }).catch(e => {
                 reject(e);
             });
